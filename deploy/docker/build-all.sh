32b3d06f745cb264f3b6646b7b0df4b9b10a0877
YW8862 <2165628227@qq.com>
2026-02-09 20:23:03 +0800

    add somr details

--- deploy/docker/build-all.sh
+++ deploy/docker/build-all.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 # 在仓库根目录执行，构建所有后端镜像
-# 若无法访问 Docker Hub，可先设置: export BASE_IMAGE=registry.cn-hangzhou.aliyuncs.com/library/ubuntu:22.04
+# 默认使用国内镜像源，避免 Docker Hub 超时；海外环境可: export BASE_IMAGE=ubuntu:22.04
 set -e
 cd "$(dirname "$0")/../.."
 
 REGISTRY="${REGISTRY:-swift}"
 TAG="${TAG:-latest}"
-BASE_IMAGE="${BASE_IMAGE:-ubuntu:22.04}"
+BASE_IMAGE="${BASE_IMAGE:-registry.cn-hangzhou.aliyuncs.com/library/ubuntu:22.04}"
 
 for target in authsvr onlinesvr friendsvr chatsvr filesvr zonesvr gatesvr; do
   echo "Building $REGISTRY/$target:$TAG ..."
