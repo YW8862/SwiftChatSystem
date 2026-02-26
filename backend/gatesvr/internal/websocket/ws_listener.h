f0a8c1a41bb0d92fe38a7f5035db11734a241df3
vviyang <2165628227@qq.com>
2026-02-27 02:13:09 +0800

    add some details for wsListener

--- backend/gatesvr/internal/websocket/ws_listener.h
+++ backend/gatesvr/internal/websocket/ws_listener.h
@@ -11,8 +11,6 @@ class GateService;
 class WebSocketHandler;
 
 namespace beast = boost::beast;
-namespace http = beast::http;
-namespace websocket = beast::websocket;
 namespace net = boost::asio;
 using tcp = boost::asio::ip::tcp;
 
